9a7619a1670983efab72ef853d70ca640d572730
hirofumi setaka <gun_seta@yahoo.co.jp>
2023-05-09 23:32:16 +0900

    スクロールした場合のみ表示するように修正

--- js/action.js
+++ js/action.js
@@ -7,6 +7,25 @@ function pagetopper_event() {
     );
 }
 
+window.addEventListener('load', (event) => {
+    const tl = document.getElementById('pagetopper_link');
+    if (window.scrollY >= 50) {
+        tl.style.setProperty('display', 'flex');
+    }
+    else {
+        tl.style.setProperty('display', 'none');
+    }
+    window.addEventListener('scroll', function () {
+        const tl = document.getElementById('pagetopper_link');
+        if (window.scrollY >= 50) {
+            tl.style.setProperty('display', 'flex');
+        }
+        else {
+            tl.style.setProperty('display', 'none');
+        }
+    });
+});
+
 const body_element = document.getElementsByTagName('body')[0];
 //吹き出し用のpタグを作成
 const new_element = document.createElement('span');
